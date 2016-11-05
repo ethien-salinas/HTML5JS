f8c439987dec8f5d9e536a2c3f43a60c348b92dc
ethien-salinas <ethien.salinas@gmail.com>
2016-11-05 12:52:44 -0600

    ADD body-parser support

--- my-personal-page/server.js
+++ my-personal-page/server.js
@@ -1,17 +1,25 @@
-const http = require('http');
+var express      = require('express')
+var bodyParser   = require('body-parser')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var app = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if(req.url === '/mail/process'){
-  	res.end('Mail process path\n');
-  }
-  res.end('Hello World\n');
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res) => {
+  res.send('GET Hello World!')
+})
+
+app.post('/', (req, res) => {
+  res.send('POST Hello World!')
+})
+
+app.post('/mail/process', (req, res) => {
+  res.send(JSON.stringify(req.body))
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
